dfd451a3624b3670c38c55dc7c53293e9354cf27
Henry-Hong <hhr8540@naver.com>
2022-07-27 13:20:18 +0900

    feat: add layout of signup page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { GlobalStyles } from "./commonStyles";
 
 import TestPage from "./routes/TestPage";
 import LoginPage from "./routes/LoginPage";
+import SignupPage from "./routes/SignupPage";
 import ErrorPage from "./routes/ErrorPage";
 import LandingPage from "./routes/LandingPage";
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/test" element={<TestPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
---
+++ src/routes/SignupPage.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, memo } from "react";
+import styled from "styled-components";
+
+// components
+import LoginInput from "../components/Login/LoginInput";
+import LoginButton from "../components/Login/LoginButton";
+import SocialLoginSection from "../components/Login/SocialLoginSection";
+
+// styled-components
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export function SignupPage() {
+  return (
+    <>
+      <Wrapper>
+        <SocialLoginSection></SocialLoginSection>
+        <LoginInput caption="이메일" placeholder="이메일을 입력해주세요"></LoginInput>
+        <LoginInput caption="비밀번호" placeholder="비밀번호를 입력해주세요"></LoginInput>
+        <LoginInput caption="비밀번호 확인" placeholder="비밀번호를 입력해주세요"></LoginInput>
+        <LoginButton>LOGIN</LoginButton>
+      </Wrapper>
+    </>
+  );
+}
+
+export default memo(SignupPage);
