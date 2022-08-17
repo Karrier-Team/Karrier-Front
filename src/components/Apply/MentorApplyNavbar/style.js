cf1ae6e1de63ba2f5f77acf7ec73306709bcfeab
Henry-Hong <hhr8540@naver.com>
2022-08-17 22:35:22 +0900

    fix: minor changes to MentorApplyPage design

--- src/components/Apply/MentorApplyNavbar/style.js
+++ src/components/Apply/MentorApplyNavbar/style.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export const Wrapper = styled.div`
   background-color: #f0f0f0;
-  border-bottom: 1px solid #000;
+  border-bottom: 1px solid var(--line-color);
   /* border-bottom: 1px solid #e1e1e1; */
   width: 100%;
   height: 10vh;
@@ -19,10 +19,11 @@ export const StyledNavLink = styled(NavLink)`
   margin: 5%;
 
   &:hover {
-    color: skyblue;
+    color: #007bc3;
   }
 
   &.active {
     color: #007bc3;
+    font-weight: bold;
   }
 `;
