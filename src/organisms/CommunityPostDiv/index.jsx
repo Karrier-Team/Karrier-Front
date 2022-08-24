fa9a90a712e112eed0f274a6a48644d51a6107f9
Henry-Hong <hhr8540@naver.com>
2022-08-24 19:57:49 +0900

    feat: Add Arrow to CommunityPostDiv

--- src/organisms/CommunityPostDiv/index.jsx
+++ src/organisms/CommunityPostDiv/index.jsx
@@ -1,17 +1,42 @@
 import * as S from "./style";
+import { useState, useRef } from "react";
 import { Text, Space } from "@mantine/core";
 import CircleWithText from "../../components/molecules/CircleWithText";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const CommunityPostDiv = ({ data: question }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const textRef = useRef();
+
   return (
     <S.Wrapper>
       <S.RowWrapper>
-        <S.ColWrapper>
+        <S.ColWrapper clicked={clicked}>
           <Text size="2em">{question.title}</Text>
           <Space h="xs"></Space>
-          <Text size="1em" color={"gray"}>
+          <Text
+            ref={textRef}
+            className={clicked ? "onClicked" : "notClicked"}
+            size="1em"
+            color={"gray"}
+          >
             {question.content}
           </Text>
+          {clicked ? (
+            <FontAwesomeIcon
+              onClick={() => setClicked((prev) => !prev)}
+              className="icon"
+              icon={faChevronUp}
+            />
+          ) : (
+            <FontAwesomeIcon
+              onClick={() => setClicked((prev) => !prev)}
+              className="icon"
+              icon={faChevronDown}
+            />
+          )}
         </S.ColWrapper>
         <S.ColWrapperCntr>
           <CircleWithText upper={question.question_like_no} lower={"좋아요"} />
--- src/organisms/CommunityPostDiv/style.js
+++ src/organisms/CommunityPostDiv/style.js
@@ -13,13 +13,35 @@ export const RowWrapper = styled.div`
   box-sizing: border-box;
   display: flex;
   width: 100%;
-  height: 10em;
   background-color: var(--bg-color-l);
 `;
 
 export const ColWrapper = styled.div`
   ${FlexCol};
   width: 90%;
+  position: relative;
+  .icon {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateY(0.7rem);
+  }
+
+  .content {
+    transition: height 3s;
+  }
+
+  .notClicked {
+    width: 100%;
+    height: auto;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  .onClicked {
+    height: 10em;
+  }
 `;
 
 export const ColWrapperCntr = styled.div`
