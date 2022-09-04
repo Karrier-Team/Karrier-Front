15593d931461313e60df506d896c32ab56d80e91
Henry-Hong <hhr8540@naver.com>
2022-09-04 14:41:56 +0900

    fix: dupliacated program name issue

--- src/routes/Mypage/MypageReviewsPage.jsx
+++ src/routes/Mypage/MypageReviewsPage.jsx
@@ -5,6 +5,7 @@ import ControllBar from "../../organisms/ControllBar/index.jsx";
 import * as S from "./style.js";
 
 import { Text, Modal } from "@mantine/core";
+import Selector from "../../components/atoms/Selector/index.jsx";
 
 const sortTypeOptions = [
   { value: "latest", name: "최신순" },
@@ -132,44 +133,29 @@ const dummyData = [
     nickname: "행복한바지",
     title: "수강후기 제목",
     content: "수강후기 내용 ",
-    comment: false,
+    comment: true,
     registerDate: "2022-08-24T21:27:42.096914",
     reviewLikeNo: 0,
   },
 ];
 
-// const dummyData = {
-//   program_no: "1",
-//   title: "고3 대비 컴퓨터 공학과의 진실",
-//   questions: [
-//     {
-//       question_no: "1",
-//       nickname: "소식돼지",
-//       title: "경북대학교 컴퓨터 공학과 세부 학과는 뭐가있나요?",
-//       content:
-//         "안녕하세요 현재 컴퓨터공학과에 진학하고 싶어하는 고3입니다. 안녕하세요 현재 컴퓨터공학과에 진학하고 싶어하는 고3입니다. 안녕하세요 현재 컴퓨터공학과에 진학하고 싶어하는 고3입니다. 블라블라 블라블라 블라블라 블라블라 블라블라 블라블라 블라블라 블라블라",
-//       modify_date: new Date().toLocaleDateString() + "",
-//       question_like_no: "10",
-//       solve: "true",
-//     },
-//     {
-//       question_no: "2",
-//       nickname: "소식돼지",
-//       title: "경북대학교 컴퓨터 공학과 세부 학과는 뭐가있나요?",
-//       content:
-//         "안녕하세요 현재 컴퓨터공학과에 진학하고 싶어하는 고3입니다. 저는 평소에 컴퓨터에 대해 좋아하고 관심이 참 많은데요..",
-//       modify_date: new Date().toLocaleDateString() + "",
-//       question_like_no: "9",
-//       solve: "false",
-//     },
-//   ],
-// };
+function makeProgramList(data) {
+  const programList = [];
+  data.forEach((question) => {
+    if (!programList.includes(question.programName))
+      programList.push(question.programName);
+  });
+  return programList.map((e) => {
+    return { name: e };
+  });
+}
 
 function MypageQnAPage() {
   const [sortType, setSortType] = useState("latest");
   const [searchType, setSearchType] = useState("content");
   const [searchValue, setSearchValue] = useState("");
   const [isModalOpened, setIsModalOpened] = useState(false);
+  const [curProgram, setCurProgram] = useState("");
 
   return (
     <>
@@ -178,7 +164,7 @@ function MypageQnAPage() {
       </CommunityNavbar>
       <S.Wrapper>
         <Text style={{ marginTop: "0.5em" }} weight={"bold"} size="2rem">
-          {"MY 질의응답"}
+          {"MY 수강후기"}
         </Text>
         <ControllBar
           type="reviews"
@@ -193,7 +179,13 @@ function MypageQnAPage() {
           onChangeSearchValue={setSearchValue}
           searchTypeOptions={searchTypeOptions}
         ></ControllBar>
-        <div>여기에 셀렉트 들어가야함</div>
+        <div style={{ width: "30rem" }}>
+          <Selector
+            name={"프로그램"}
+            handleChange={setCurProgram}
+            options={makeProgramList(dummyData)}
+          ></Selector>
+        </div>
         <Modal
           size="60%"
           centered
@@ -218,9 +210,20 @@ function MypageQnAPage() {
             </div>
           </S.CenterWrapper>
         </Modal>
-        {dummyData.map((question) => (
-          <CommunityPostDiv data={question} />
-        ))}
+        {dummyData
+          .filter((review) => {
+            if (curProgram === "")
+              return true; // 아직 선택된 프로그램이 없을때는 모든 질의응답을 보여줌.
+            else return review.programName === curProgram;
+          })
+          // .sort() 이친구는 나중에 구현해야지!
+          .map((review) => (
+            <CommunityPostDiv
+              type="reviews"
+              key={review.reviewNo}
+              data={review}
+            />
+          ))}
       </S.Wrapper>
     </>
   );
