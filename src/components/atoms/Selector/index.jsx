081ca61096b2e277b3e00f7fdb4ff82f3ad0f9c7
Henry-Hong <hhr8540@naver.com>
2022-09-07 16:52:01 +0900

    Fix: atoms-Selector (It gets option object like {id, name})

--- src/components/atoms/Selector/index.jsx
+++ src/components/atoms/Selector/index.jsx
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
 import * as S from "./style";
 
-// TODO: Selector도 localStorage에 저장되어있으면 selected 설정을 해줘야함
 function Selector({ name, handleChange, options }) {
   const [selected, setSelected] = useState(false);
-
   return (
     <S.Selector
       onChange={(e) => {
-        handleChange(e.target.value);
+        handleChange(parseInt(e.target.value));
         setSelected(true);
       }}
       selected={selected}
-      style={{ fontWeight: "bold" }}
     >
       <option disabled selected value="">
         {name + "을 입력해주세요."}
       </option>
       {options.map((option) => (
-        // value={option.value} -> value={option.name}
-        <option value={option.name}>{option.name}</option>
+        <option key={option.id} value={option.id}>
+          {option.name}
+        </option>
       ))}
     </S.Selector>
   );
