f9a0212f752bfcdb704085ae342ff8452073b1b1
Henry-Hong <hhr8540@naver.com>
2022-09-11 14:31:49 +0900

    feat: add useAuth hooks

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
+// 이름은 Provider인데, 왜 export default는 Auth Context냐;;
