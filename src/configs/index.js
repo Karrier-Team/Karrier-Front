62fbde53b654b2e9f2a3d1c32c6ace3ecbdfef62
Henry-Hong <hhr8540@naver.com>
2022-10-14 22:09:45 +0900

    fix: fix baseUrl

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,7 +1,5 @@
 const config = {
-  baseURL: "http://3.38.34.99:8080",
-  mentorEmail: "jymoon1121@gmail.com",
-  password: "qwer1234!",
+  baseURL: "https://www.karrier.ml",
 };
 
 export default config;
